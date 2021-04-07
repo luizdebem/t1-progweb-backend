7b423bcef166d01c328f395314e0914cca8d17bd
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-04-06 22:41:09 -0300

    better error messages

--- src/main/java/com/luiz/trabalhofinal/usuario/UsuarioService.java
+++ src/main/java/com/luiz/trabalhofinal/usuario/UsuarioService.java
@@ -1,7 +1,9 @@
 package com.luiz.trabalhofinal.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -26,7 +28,9 @@ public class UsuarioService {
     }
 
     public void updateUsuario(Usuario usuario) {
-        Usuario usuarioExistente = usuarioRepository.findById(usuario.getId()).orElseThrow(() -> new IllegalStateException("Usuário não encontrado."));
+        Usuario usuarioExistente = usuarioRepository.findById(usuario.getId()).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Usuário não encontrado."));
+
         usuarioExistente.setCPF(usuario.getCPF());
         usuarioExistente.setDataDeNascimento(usuario.getDataDeNascimento());
         usuarioExistente.setEstadoCivil(usuario.getEstadoCivil());
@@ -35,16 +39,17 @@ public class UsuarioService {
         usuarioExistente.setNomeCompleto(usuario.getNomeCompleto());
         usuarioExistente.setSexo(usuario.getSexo());
         usuarioRepository.save(usuarioExistente);
+
     }
 
     public void deleteUsuario(Long usuarioId) {
         boolean usuarioExiste = usuarioRepository.existsById(usuarioId);
 
         if (!usuarioExiste) {
-            throw new IllegalStateException("Usuário não encontrado.");
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Usuário não encontrado.");
         }
 
         usuarioRepository.deleteById(usuarioId);
-
     }
 }
