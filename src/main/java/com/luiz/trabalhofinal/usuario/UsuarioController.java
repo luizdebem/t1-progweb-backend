ad60b48ba56032caf100b6c2de65b8f2bc6406bd
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-04-08 20:43:30 -0300

    CORS settings

--- src/main/java/com/luiz/trabalhofinal/usuario/UsuarioController.java
+++ src/main/java/com/luiz/trabalhofinal/usuario/UsuarioController.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
